824b38bffd44de46970915448329ce89926acc0c
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2018-05-14 08:14:21 -0700

    Add Leiningen 'testall' alias to test with Clojure version 1.3 thru 1.9

--- project.clj
+++ project.clj
@@ -4,4 +4,14 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [clojail "1.0.3"]]
+  :aliases {"testall" ["with-profile" "dev,1.3,default:dev,1.4,default:dev,1.5,default:dev,1.7,default:dev,1.8,default:dev,1.9,default" "test"]}
+  :profiles {
+             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
+             }
   :jvm-opts ["-Djava.security.policy=example.policy"])
--- test/findfn/core_test.clj
+++ test/findfn/core_test.clj
@@ -3,13 +3,32 @@
         [clojail.testers :only [secure-tester]]
         findfn.core))
 
+(defn clj-version []
+  ((juxt :major :minor) *clojure-version*))
+
+(defn min-clj-version [min-version-vector]
+  (>= (compare (clj-version) min-version-vector) 0))
+
+(defn max-clj-version [min-version-vector]
+  (<= (compare (clj-version) min-version-vector) 0))
+
 (deftest find-fn-test
   (testing "that it works."
     (is (= (find-fn secure-tester "lolomg" "lol" "omg") '[clojure.core/str]))))
 
 (deftest find-arg-test
   (testing "that it works"
-    (is (= (find-arg secure-tester ["a" "b" "c"] 'map '% [\a \b \c])
-           '[clojure.string/trim clojure.string/lower-case
-             clojure.core/munge clojure.core/print-str clojure.core/str
-             clojure.core/namespace-munge]))))
+    (is (= (set (find-arg secure-tester ["a" "b" "c"] 'map '% [\a \b \c]))
+           (set (concat
+                 ;; trim used to take characters and return strings in
+                 ;; Clojure 1.5.1 and earlier (probably by accident,
+                 ;; not design), but starting with Clojure 1.6.0 it
+                 ;; only accepts strings as arguments.
+                 (if (max-clj-version [1 5])
+                   '[clojure.string/trim])
+                 ;; re-quote-replacement was added in Clojure 1.5.0
+                 (if (min-clj-version [1 5])
+                   '[clojure.string/re-quote-replacement])
+                 '[clojure.string/lower-case
+                   clojure.core/munge clojure.core/print-str clojure.core/str
+                   clojure.core/namespace-munge]))))))
